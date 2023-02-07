209dfc78b7829eed7344fa0a461a52bccbf828d9
TOm <thomas.hogarth@gmail.com>
2023-02-07 15:08:00 +0000

    use 8 gpu on server

--- scripts/run_homes_semseg.sh
+++ scripts/run_homes_semseg.sh
@@ -2,7 +2,7 @@
 
 export task=semantic
 
-traindir="./output/synthhomes_semseg"
+traindir="./output/synthhomes_v3_640_semseg_lr10-e4"
 lastcheckpoint=`cat $traindir/last_checkpoint`
 lastname=${lastcheckpoint%????}
 outputdir="${traindir}/${lastname}"
