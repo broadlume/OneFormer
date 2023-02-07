1f4a3d57664ed4871a0bb242dbf40f3053b18643
TOm <thomas.hogarth@gmail.com>
2023-02-07 13:08:03 +0000

    adjusted scripts to run on server

--- scripts/fetch_datasets.sh
+++ scripts/fetch_datasets.sh
@@ -1,4 +1,4 @@
-DATASET_DIR="../../"
+DATASET_DIR="/mnt/mlvolume/"
 OUTPUT_PATH="${DATASET_DIR}datasets.zip"
 
 wget -O $OUTPUT_PATH https://www.dropbox.com/s/5utaqvbokky6kst/datasets.zip?dl=1
---
+++ scripts/srvr_train_homes_semseg.sh
@@ -0,0 +1,7 @@
+export DETECTRON2_DATASETS=/mnt/mlvolume/datasets
+
+python ./train_net.py --dist-url 'tcp://127.0.0.1:50163' \
+    --num-gpus 2 \
+    --config-file ./configs/synthhomes/oneformer_convnext_large_bs16_160k.yaml \
+    --resume \
+    OUTPUT_DIR /mnt/mlvolume/output/synthhomes_v3_640_semseg_lr10-e4 SOLVER.BASE_LR 1.0e-04
