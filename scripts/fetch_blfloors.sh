02f640000ed7ab5eaed945c30190fc3962dbeddc
TOm <thomas.hogarth@gmail.com>
2023-02-10 16:25:55 +0000

    set path for blfloors dataset

--- scripts/fetch_blfloors.sh
+++ scripts/fetch_blfloors.sh
@@ -1,5 +1,5 @@
 DATASET_DIR="/mnt/mlvolume/datasets/vmdatasets"
 OUTPUT_PATH="${DATASET_DIR}blfloors.zip"
 
-wget -O $OUTPUT_PATH https://www.dropbox.com/s/5utaqvbokky6kst/datasets.zip?dl=1
+wget -O $OUTPUT_PATH https://www.dropbox.com/s/lydzu7dfh8806il/blfloors.zip?dl=1
 unzip $OUTPUT_PATH -d $DATASET_DIR
